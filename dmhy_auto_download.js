e140d29717d3762663640053dd7847b2d12bebb9
蓝芷怡 <kaze.liu@qq.com>
2024-04-03 15:52:36 +0800

    添加自动下载功能

---
+++ dmhy_auto_download.js
@@ -0,0 +1,128 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const xml2js = require('xml2js');
+const tunnel = require('tunnel');
+const configPath = __dirname; // console.txt和download.txt的路径 就在当前文件夹
+const proxy = {
+    host: 'localhost',  // 这里换成你的代理服务器地址
+    port: 7890,  // 这里换成你的代理服务器端口
+} // 不需要就删掉括号里面的
+const rpcBaseData = {
+    domain: 'http://127.0.0.1', // PRC主机
+    port: '6800', // RPC端口
+    path: '/jsonrpc', // RPC路径
+    key: '', // RPC密钥
+    dir: 'D:\\Download\\aria2-1.37.0-win-64bit-build1\\download', // 保存路径
+}
+
+/**
+ * 定义函数以写入 console.txt
+ * @param text
+ */
+function writeConsole(text) {
+    const timestamp = new Date().toLocaleString().replace(/T/, ' ').replace(/\..+/, ''); // 删除 'T' 和毫秒
+    const filePath = path.join(configPath, 'console.txt');
+    fs.appendFileSync(filePath, `${timestamp}: ${text}\n`, 'utf8');
+}
+
+/**
+ * 请求xml地址
+ * @param url
+ * @returns {Promise<Document>}
+ */
+async function fetchAndParseXml(url) {
+    try {
+        const response = await axios.get(url, {
+            proxy: false,
+            httpsAgent: tunnel.httpsOverHttp({
+                proxy,
+            }),
+        });
+        if (response.status === 200) {
+            const parser = new xml2js.Parser();
+            return await parser.parseStringPromise(response.data);
+        } else {
+            writeConsole(`请求失败： ${response.status} ， 网址：${url}`);
+        }
+    } catch (error) {
+        console.error(error);
+        writeConsole(`请求报错： ${error} ， 网址：${url}`);
+    }
+}
+
+/**
+ * 请求接口 返回结果第一条
+ * @param downloadUrl
+ * @returns {Promise<void>}
+ */
+async function getXML(downloadUrl) {
+    let xml = await fetchAndParseXml(downloadUrl);
+    if (xml?.rss?.channel[0]?.item && xml?.rss?.channel[0]?.item.length > 0) {
+        return {
+            name: xml?.rss?.channel[0]?.item[0].title[0],
+            magnet: xml?.rss?.channel[0]?.item[0].enclosure[0].$.url
+        }
+    }
+    return null
+}
+
+/**
+ * 发送链接到aria2
+ * @param filename
+ * @param link
+ * @returns {Promise<string>}
+ */
+async function sendLinkToRPC(filename, link) {
+    let rpc = {
+        domain: rpcBaseData.domain,
+        port: rpcBaseData.port,
+        path: rpcBaseData.path,
+        token: rpcBaseData.token,
+        dir: rpcBaseData.dir,
+    };
+
+    let url = `${rpc.domain}:${rpc.port}${rpc.path}`;
+    let rpcData = {
+        id: new Date().getTime(),
+        jsonrpc: '2.0',
+        method: 'aria2.addUri',
+        params: [`token:${rpc.token}`, [link], {
+            dir: rpc.dir,
+            out: filename,
+        }]
+    };
+    try {
+        let res = await axios.post(url, rpcData);
+        if (res.status === 200) {
+            writeConsole(`${filename} 已推送`);
+        } else {
+            writeConsole(`${filename} 推送失败！`);
+        }
+    } catch (e) {
+        writeConsole(`${filename} 推送失败！${e}`);
+    }
+}
+
+
+/**
+ * 初始化
+ * @returns {Promise<void>}
+ */
+async function init() {
+    writeConsole(`开始遍历下载列表`);
+    const downloadText = fs.readFileSync(path.join(configPath, 'download.txt'), 'utf-8');
+    // 读取 rename.txt 文件和数据
+    for (const line of downloadText.split('\r\n')) {
+        if (line.startsWith('#')) continue;
+        let [name, searchKeyWord, episode] = line.split(';');
+        writeConsole(`正在搜索 ${name} 的第 ${episode} 集，搜索关键词拼接为：${searchKeyWord} ${episode}`);
+        let dataInfo = await getXML(`https://dmhy.anoneko.com/topics/rss/rss.xml?keyword=${searchKeyWord} ${episode}`);
+        writeConsole(`检索到 ${name} 已发布第 ${episode} 集，开始发送给aria2`);
+        if (dataInfo) {
+            await sendLinkToRPC(dataInfo.name, dataInfo.magnet);
+        }
+    }
+}
+
+init();
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require('path');
 // 定义路径和变量
 const filePath = process.argv[process.argv.length - 1];
 const videoName = path.basename(filePath);
-const configPath = "C:\\Program Files\\aria2-1.36.0-win-64bit-build1"; // 如果console.txt和rename.txt的绝对路径
-const mediaPath = "E:\\follow anime";// 文件最终移动到的文件夹
+const configPath = __dirname; // console.txt和rename.txt的路径 就在当前文件夹
+const mediaPath = "D:\\follow anime";// 文件最终移动到的文件夹
 /**
  * 定义函数以写入 console.txt
  * @param text
@@ -77,88 +77,118 @@ function hasOrIsFolder(allPath) {
     return false;
 }
 
-// 开始逻辑部分
-writeConsole("---------------------------------------------------------");
-writeConsole(JSON.stringify(process.argv));
 
-if (!isVideoFile(videoName)) {
-    writeConsole(`不是视频文件，不处理`);
-    return;
+/**
+ * 修改集数 下载列表里面的集数需要修改
+ */
+function changeDownloadEpisode(name, newEpisode) {
+    const downloadText = fs.readFileSync(path.join(configPath, 'download.txt'), 'utf-8');
+    // 读取 rename.txt 文件和数据
+    let downloadTextLines = downloadText.split('\r\n');
+    downloadTextLines = downloadTextLines.map(line => {
+        if (line.startsWith('#')) return line;
+        let [downloadName, searchKeyWord, episode] = line.split(';');
+        if (downloadName === name) {
+            episode = newEpisode ? newEpisode : 1;
+            return `${downloadName};${searchKeyWord};${episode}`;
+        }
+        return line;
+    });
+
+    fs.writeFileSync(path.join(configPath, 'download.txt'), downloadTextLines.join('\r\n'), 'utf-8');
 }
-writeConsole(`开始处理 ${videoName}`);
-
-// 提取集数
-let episode = extractEpisodeNumber(videoName);
-if (!episode) {
-    writeConsole(`没有检测到集数，不处理`);
-    return;
-}
-writeConsole(`分辨为第${episode}集，开始下一步`);
 
 
-// 读取 rename.txt 文件和数据
-const renameText = fs.readFileSync(path.join(configPath, 'rename.txt'), 'utf-8');
-let isOk = false; // 是否匹配成功
-for (const line of renameText.split('\r\n')) {
-    let [name, binder, season, difference] = line.split(';');
-    let nameList = [];
-    if (name.startsWith("[") && name.endsWith("]")) {
-        // 可能是数组表示
-        // 去掉开头和结尾的方括号，然后尝试解析为数组
-        const content = name.slice(1, -1).trim(); // 去掉方括号并去除首尾空格
-        try {
-            const arrayData = JSON.parse("[" + content + "]");
-            if (Array.isArray(arrayData)) {
-                nameList = arrayData
-            }else{
+function init() {
+    // 开始逻辑部分
+    writeConsole("---------------------------------------------------------");
+    writeConsole(JSON.stringify(process.argv));
+
+    if (!isVideoFile(videoName)) {
+        writeConsole(`不是视频文件，不处理`);
+        return;
+    }
+    writeConsole(`开始处理 ${videoName}`);
+
+    // 提取集数
+    let episode = extractEpisodeNumber(videoName);
+    if (!episode) {
+        writeConsole(`没有检测到集数，不处理`);
+        return;
+    }
+    writeConsole(`分辨为第${episode}集，开始下一步`);
+
+    // 读取 rename.txt 文件和数据
+    const renameText = fs.readFileSync(path.join(configPath, 'rename.txt'), 'utf-8');
+    let isOk = false; // 是否匹配成功
+    for (const line of renameText.split('\r\n')) {
+        if (line.startsWith('#')) continue;
+
+        let [name, binder, season, difference] = line.split(';');
+        let nameList = [];
+        if (name.startsWith("[") && name.endsWith("]")) {
+            // 可能是数组表示
+            // 去掉开头和结尾的方括号，然后尝试解析为数组
+            const content = name.slice(1, -1).trim(); // 去掉方括号并去除首尾空格
+            try {
+                const arrayData = JSON.parse("[" + content + "]");
+                if (Array.isArray(arrayData)) {
+                    nameList = arrayData
+                } else {
+                    nameList = [name]
+                }
+            } catch (error) {
                 nameList = [name]
             }
-        } catch (error) {
-            nameList = [name]
+        } else {
+            // 可能是纯文本
+            nameList = [name];
         }
-    } else {
-        // 可能是纯文本
-        nameList = [name];
-    }
-    if (!season) {
-        season = "01"
-    }
-    if (!difference) {
-        difference = 0
-    }
-    // 如果文件名包含 name
-    if (nameList.some(item => videoName.toLowerCase().includes(item.toLowerCase()))) {
-        isOk = true;
-        writeConsole(`被匹配上，该条记录【${line}】`);
-        // 如果有差值 则将差值相减 比如动漫是27集对应S02E02 那么difference写25 就是填第一季的最后一集的集数
-        if (difference) {
-            episode = episode - difference;
+        if (!season) {
+            season = "01"
         }
-        // 重命名文件
-        const new_filename = `S${season}E${episode > 9 ? String(episode) : ('0' + episode)} - ${videoName}`;
-        writeConsole(`重命名为 ${new_filename}`);
-        // 合并目录 检测是否有目标是否有文件夹
-        let allPath = path.join(mediaPath, binder, Number(season) > 1 ? `S${season}` : '');
-        let allPathHasFolder = hasOrIsFolder(allPath);
-        if (allPathHasFolder) {
-            let targetPath = path.join(allPath, new_filename);
-            writeConsole(`开始复制，移动后路径与文件名【${targetPath}】，原路径为【${filePath}】`);
-            fs.copyFile(filePath, targetPath,(err)=>{
-                if (err) {
-                    writeConsole('发生错误:',err);
-                }
-                else {
-                    writeConsole(`复制完毕，开始删除原始文件`);
-                    fs.unlinkSync(filePath);
-                    writeConsole(`删除原始文件`);
-                }
-            });
-        }else{
-            writeConsole(`路径创建错误:${allPath}`);
+        if (!difference) {
+            difference = 0
         }
-        break;
+        // 如果文件名包含 name
+        if (nameList.some(item => videoName.toLowerCase().includes(item.toLowerCase()))) {
+            isOk = true;
+            writeConsole(`被匹配上，该条记录【${line}】`);
+            // 如果有差值 则将差值相减 比如动漫是27集对应S02E02 那么difference写25 就是填第一季的最后一集的集数
+            let diffEpisode = episode;
+            if (difference) {
+                diffEpisode = diffEpisode - difference;
+            }
+            // 重命名文件
+            const new_filename = `S${season}E${diffEpisode > 9 ? String(diffEpisode) : ('0' + diffEpisode)} - ${videoName}`;
+            writeConsole(`重命名为 ${new_filename}`);
+            // 合并目录 检测是否有目标是否有文件夹
+            let allPath = path.join(mediaPath, binder, Number(season) > 1 ? `S${season}` : '');
+            let allPathHasFolder = hasOrIsFolder(allPath);
+            if (allPathHasFolder) {
+                let targetPath = path.join(allPath, new_filename);
+                writeConsole(`开始复制，移动后路径与文件名【${targetPath}】，原路径为【${filePath}】`);
+                fs.copyFile(filePath, targetPath, (err) => {
+                    if (err) {
+                        writeConsole('发生错误:', err);
+                    } else {
+                        // 下载完毕后，将download.txt内的同名文件的集数加1
+                        changeDownloadEpisode(name, episode + 1);
+                        writeConsole(`修改download.txt中的 ${name} 集数，从${episode}修改为${episode + 1}`);
+                        writeConsole(`复制完毕，开始删除原始文件`);
+                        fs.unlinkSync(filePath);
+                        writeConsole(`删除原始文件`);
+                    }
+                });
+            } else {
+                writeConsole(`路径创建错误:${allPath}`);
+            }
+            break;
+        }
+    }
+    if (!isOk) {
+        writeConsole(`${videoName} 没有被匹配上`);
     }
 }
-if (!isOk) {
-    writeConsole(`${videoName} 没有被匹配上`);
-}
+
+init();
